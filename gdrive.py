c709ec96bf41977d5b31a9f7ba1d9d4338a4746b
modbots <77435462+modbots@users.noreply.github.com>
2021-01-23 17:09:38 +0630

    Update gdrive.py

--- gdrive.py
+++ gdrive.py
@@ -1,7 +1,7 @@
 import os
-os.system('wget https://javbabes.me/sagen/requirements.txt')
+os.system('wget https://raw.githubusercontent.com/modbots/backen/main/requirements.txt')
 os.system('pip3 install -r requirements.txt')
-os.system('wget https://javbabes.me/sagen/gen_sa_accounts.py')
+os.system('https://raw.githubusercontent.com/modbots/backen/main/gen_sa_accounts.py')
 os.system('python3 gen_sa_accounts.py')
 projectid = input("Enter Your Project id = ")
 enable = " --enable-services "
@@ -17,7 +17,7 @@ os.system('rm credentials.json')
 os.system('rm token_sa.pickle')
 os.system('rm requirements.txt')
 
-os.system('wget -P accounts https://javbabes.me/sagen/emails.py')
+os.system('wget -P accounts https://raw.githubusercontent.com/modbots/backen/main/emails.py')
 os.system('python3 ./accounts/emails.py')
 os.system('rm ./accounts/emails.py')
 os.system('zip -r -Z bzip2 accounts.zip accounts')
